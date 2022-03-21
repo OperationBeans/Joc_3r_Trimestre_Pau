e27abab7d78ccd5ccef616d05fe8bb19d5fb02e9
OperationBeans <101245841+OperationBeans@users.noreply.github.com>
2022-03-21 12:21:46 +0100

    Creació de personatge Animat
    Hem creat un personatge animat amb Animated Sprite, i hem implementat una càmara per seguir-lo.

--- Personatge.gd
+++ Personatge.gd
@@ -6,7 +6,6 @@ var gravetat = Vector2.DOWN * 980
 var salt = Vector2.UP * 575
 
 func _physics_process(delta):
-	rotation_degrees = 180
 	velocitat.x = 0
 	if Input.is_action_pressed("mou_dreta"):
 		velocitat += Vector2.RIGHT * velocitat_base
@@ -18,3 +17,18 @@ func _physics_process(delta):
 		velocitat += Vector2.DOWN * velocitat_base
 	velocitat += gravetat * delta
 	velocitat = move_and_slide(velocitat, Vector2.UP)
+	anima(velocitat)
+func anima(velocitat: Vector2):
+	var animacio = $AnimatedSprite
+	if velocitat.x > 0:
+		animacio.flip_h = false
+		animacio.play("camina")
+	elif velocitat.x < 0:
+		animacio.flip_h = true
+		animacio.play('camina')
+	
+	if velocitat.y < -1:
+		animacio.play("salta")
+		
+	if abs(velocitat.x) < 0.5:
+		animacio.play('default')
