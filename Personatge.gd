9dd6ef2424e9797b45a49a5ed788e6a28643c02a
OperationBeans <101245841+OperationBeans@users.noreply.github.com>
2022-03-25 12:19:37 +0100

    Creació de lava i ampliació del terreny

--- Personatge.gd
+++ Personatge.gd
@@ -1,10 +1,11 @@
 extends KinematicBody2D
 var vida = 100
-
+var dins = false
 var velocitat_base = 200
 var velocitat = Vector2.ZERO
 var gravetat = Vector2.DOWN * 980
-var salt = Vector2.UP * 575
+var salt = Vector2.UP * 450
+var segons_dins = 0
 
 func _physics_process(delta):
 	velocitat.x = 0
@@ -19,6 +20,8 @@ func _physics_process(delta):
 	velocitat += gravetat * delta
 	velocitat = move_and_slide(velocitat, Vector2.UP)
 	anima(velocitat)
+	if vida <=0 :
+		print('Mort')
 func anima(velocitat: Vector2):
 	var animacio = $AnimatedSprite
 	if velocitat.x > 0:
@@ -45,3 +48,27 @@ func _on_Portal_abaix_body_entered(body):
 
 func _on_Portal_Dalt_body_entered(body):
 	position = Vector2(860,300)
+
+
+
+
+func _on_Area2D_body_entered(body):
+	dins = true
+	velocitat_base = 100
+	segons_dins = 0
+	modulate = Color(1,0.5,0)
+	$Timer.start()
+func _on_Timer_timeout():
+	segons_dins += 1
+	vida -= 50
+	if segons_dins % 2 != 0:
+		modulate = Color(1,1,1)
+		velocitat_base = 200
+	else:
+		modulate = Color(1,0.5,0)
+func _on_Area2D_body_exited(body):
+	segons_dins = 0
+	modulate = Color(1,1,1)
+	dins = false
+	$Timer.stop()
+
